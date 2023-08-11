99a56170465f80d5be54be1d0e142d0294c499f6
GUILHERME  LEITE <141749757+guilhermebleite@users.noreply.github.com>
2023-08-11 09:12:53 -0300

    media
    calculando média

---
+++ .idea/# media.py
@@ -0,0 +1,10 @@
+                         # media 
+nota01=float(input('Digite a primeira nota:'))
+nota02=float(input('Digite a segunda nota:'))
+media = (nota01+nota02)/2
+if media >= 7:
+    print('Parabéns, você foi aprovado, obteve média {} '.format(media))
+else:
+    print('Infelizmente, você não obteve a média')
+    
+
